5ed916e4bbca58cc06f3419d7caa6430313adcb3
ratthapon_h <ratthapon_h@outlook.com>
2016-05-06 23:53:20 +0700

    change config - none fc experiment to use the original corpus

--- Util/featread.m
+++ Util/featread.m
@@ -1,9 +1,9 @@
 function [ MFCC ] = featread( filePath , nDimension)
     fid = fopen(filePath, 'r');
-    header = fread(fid,1,'uint32');
+    header = fread(fid,1,'uint32')
     feat = fread(fid, 'float32');
     fclose(fid);
-    if nDimension == []
+    if isempty(nDimension)
         nDimension= 13;
     end
     MFCC = reshape(feat, nDimension, header/nDimension);
