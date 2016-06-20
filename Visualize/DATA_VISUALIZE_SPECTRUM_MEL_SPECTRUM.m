8d033a3ae106a4252f353dd233016fa83fa3ddee
ratthapon_h <ratthapon_h@outlook.com>
2016-06-21 00:40:02 +0700

    Rename spectrum visualization and 3D Fourier ploter.

--- Visualize/DATA_VISUALIZE_SPECTRUM_MEL_SPECTRUM.m
+++ Visualize/DATA_VISUALIZE_SPECTRUM_MEL_SPECTRUM.m
@@ -2,13 +2,13 @@
 mfccparams;
 C = 30;
 HF = 7300;
-fileList = importdata('F:\IFEFSR\AudioFC\an4traintest.txt');
+fileList = importdata('F:\IFEFSR\AudioFC\an4fileids.txt');
 fName = fileList{1}
-dat = load(['F:\IFEFSR\AudioFC\FC\AN4_8K_CURVEFIT_LM\' fName '.mat']);
+dat = load(['F:\IFEFSR\AudioFC\FC\AN4_8_75_DECIMATE_INV_QR_GPU\' fName '.mat']);
 f1 = dat.f;
-dat = load(['F:\IFEFSR\AudioFC\FC\QR\AN4_8K\' fName '.mat']);
+dat = load(['F:\IFEFSR\AudioFC\FC\AN4_8_75_DECIMATE_INV_QR_GPU\' fName '.mat']);
 f2 = dat.f;
-dat = load(['F:\IFEFSR\AudioFC\FC\QR\AN4_16K\' fName '.mat']);
+dat = load(['F:\IFEFSR\AudioFC\FC\AN4_8_75_INV_QR_GPU\' fName '.mat']);
 f3 = dat.f;
 inFName = ['F:\IFEFSR\SpeechData\an4_fir1_8_75\wav\' fName '.raw'];
 fid = fopen(inFName, 'r');
@@ -33,10 +33,10 @@ lpFilter = [1 zz-1];
 f = [0 0.4 0.41 1];            % Frequency breakpoints
 m = [1 1 1 0 ];                  % Magnitude breakpoints
 b = fir2(60,f,m);               % FIR filter design
-b1 = fir1(30,0.75);
+b1 = fir1(8,0.75);
 % freqz(b,1,512);                 % Frequency response of filter
-% sig2 = filtfilt(b1,1,sig2);       % Zero-phase digital filtering
-% sig3 = filtfilt(b1,1,sig3);       % Zero-phase digital filtering
+sig2 = filtfilt(b1,1,sig2);       % Zero-phase digital filtering
+sig3 = filtfilt(b1,1,sig3);       % Zero-phase digital filtering
 
 
 
@@ -52,21 +52,41 @@ SPEC2 = [SPEC2; zeros(128, size(SPEC2,2))];
 
 f1 = figure(1),
 subplot(1,4,1), imagesc(-SPEC1); set(gca,'YDir','normal')
-title('16 kHz signal')
-xlabel('Frame index')
-ylabel('Frequency index')
+title('Original Signal 16 kHz')
+xlabel('Time (ms)')
+ylabel('Frequency (Hz)')
+ax = gca;
+set(ax,'XTickLabel',[1:6]*320);
+set(ax,'YTick',[32:32:256]);
+set(ax,'YTickLabel',[1:8]*1000);
+
 subplot(1,4,2), imagesc(-SPEC2); set(gca,'YDir','normal')
-title('8 kHz signal')
-xlabel('Frame index')
-ylabel('Frequency index')
+title('Original Signal 8 kHz')
+xlabel('Time (ms)')
+ylabel('Frequency (Hz)')
+ax = gca;
+set(ax,'XTickLabel',[1:6]*320);
+set(ax,'YTick',[32:32:256]);
+set(ax,'YTickLabel',[1:8]*1000);
+
 subplot(1,4,3), imagesc(-SPEC3); set(gca,'YDir','normal')
-title('reconstructed 16 kHz signal')
-xlabel('Frame index')
-ylabel('Frequency index')
+title('Reconstructed Signal 16 kHz')
+xlabel('Time (ms)')
+ylabel('Frequency (Hz)')
+ax = gca;
+set(ax,'XTickLabel',[1:6]*320);
+set(ax,'YTick',[32:32:256]);
+set(ax,'YTickLabel',[1:8]*1000);
+
 subplot(1,4,4), imagesc(-SPEC4); set(gca,'YDir','normal')
-title('reconstructed 8 kHz signal')
-xlabel('Frame index')
-ylabel('Frequency index')
+title('Reconstructed Signal 8 kHz')
+xlabel('Time (ms)')
+ylabel('Frequency (Hz)')
+ax = gca;
+set(ax,'XTickLabel',[1:6]*320);
+set(ax,'YTick',[32:32:256]);
+set(ax,'YTickLabel',[1:8]*1000);
+
 colormap(gray)
 
 
--- Visualize/plot3DFourier.m
+++ Visualize/plot3DFourier.m
@@ -14,28 +14,48 @@ figure(2),plot(sig)
 %% fast fourier transform
 nfft = 2^nextpow2( size(sig,1) );     % length of FFT analysi
 hnfft = nfft/2+1;
-spectrum = abs( fft(sig,nfft,1) );
+rawSpectrum = abs( fft(sig,nfft,1) );
 
 %% 3d plot section
 K = 10; % N frequecy
 coloreMap = jet(K);  % See the help for COLORMAP to see other choices.
-figure(1),plot3([1:size(sig,1)]',repmat(0,size(sig)),(sig/2^2) ...
+figure(1),plot3([1:size(sig,1)]', repmat(0,size(sig)), (sig/2^2) ...
                 ,'linewidth',2); % origin sig
+amplitude = zeros(K,1);
+
+% filterbank to K bands.
+spectrum = zeros(K+1,1);
+chBandwidth = floor(nfft/K);
+for k = 0:K-1
+   spectrum(k+1) = sum(rawSpectrum(k*chBandwidth + 1 : (k+1)*chBandwidth));
+end
+
 for k = 1:K
     % bandpass filtering 
     bandPassSpectrum = zeros(size(spectrum));
-    bandPassSpectrum(k) = spectrum(k); % floor(nfft/K)
+    bandPassSpectrum(k+1) = spectrum(k); % floor(nfft/K)
     
     % ifft
     iSig = ifft(bandPassSpectrum,nfft,1);
+    amplitude(k) = max(abs(iSig));
     hold on, grid on;
-    figure(1),plot3([1:nfft]', repmat(k,nfft,1), iSig(1:nfft), ...
+    figure(1),plot3([1:nfft]' , repmat(k,nfft,1), iSig(1:nfft), ...
         'color',coloreMap(k,:),'linewidth',2);
-    xlabel('time'),ylabel('frequency order (0 = original)'),zlabel('spectrum magnitude');
-    
+   
 end
-figure(1),stem(spectrum);
+set(gca,'XTick',[32:32:nfft]);
+set(gca,'XTickLabel',[1:nfft/32]*2);
+set(gca,'YTick',0:K);
+freqLabel = [{'Original Signal'}];
+for i = 1:K
+    freqLabel = [freqLabel, {num2str(i * 800)}];
+end
+set(gca,'YTickLabel',freqLabel);
 
+
+stem3(zeros(K, 1), [1:K]', amplitude,'linewidth',2);
+plot3(zeros(K, 1), [1:K]', zeros(K, 1),'linewidth',2);
+xlabel('time (ms)'),ylabel('frequency (Hz)'),zlabel('spectrum magnitude');
 
 
 
