9c1f35a40eb67e6c3dcdaf8a38c356223a09cdfd
ratthapon_h <ratthapon_h@outlook.com>
2016-08-05 00:10:08 +0700

    Fix frequenncy response script.

--- FrequencyResponse/FREQUENCY_RES_OF_FC.m
+++ FrequencyResponse/FREQUENCY_RES_OF_FC.m
@@ -1,9 +1,13 @@
 %% generate signal and other information
 close all
 sig = createComplexSignal(8000);
+% sig16 = createComplexSignal(16000);
+% alpha = 0.97;
+% sig = filter( [1 -alpha], 1, sig );
 rbs = 4;
+rbs_2 = 2;
 nScale = 2;
-scaleOrder = [4 2];
+scaleOrder = [ 4 2];
 lambda = 0e4;
 Fin_out = [8000 8000];
 
@@ -11,7 +15,7 @@ Fin_out = [8000 8000];
 plotSigVsFreqRes( sig, 'Original signal', 8000 );
 
 %% interpolate
-plotSigVsFreqRes( interp(sig, 2), 'Original signal', 16000 );
+plotSigVsFreqRes( interp(sig, 2), 'Interpolate signal', 16000 );
 
 %% Fix R,D original coding section
 rangePartition = rbs*ones(size(sig, 1)/rbs, 1);
@@ -19,6 +23,13 @@ FC_QR = FixAFCEncoder( sig, rangePartition, 1, 2, lambda );
 REC_SIG_1 = decompressAudioFC(FC_QR, Fin_out(1), Fin_out(2), 15);
 caption = ['Fix R,D scale FC' ' psnr = ' num2str(PSNR(sig, REC_SIG_1))];
 plotSigVsFreqRes( REC_SIG_1, caption, Fin_out(2) );
+
+% Fix R,D original coding section
+rangePartition = rbs_2*ones(size(sig, 1)/rbs_2, 1);
+FC_QR = FixAFCEncoder( sig, rangePartition, 1, 2, lambda );
+REC_SIG_1_2 = decompressAudioFC(FC_QR, Fin_out(1), Fin_out(2), 15);
+caption = ['Fix R,D scale FC 2' ' psnr = ' num2str(PSNR(sig, REC_SIG_1_2))];
+plotSigVsFreqRes( REC_SIG_1_2, caption, Fin_out(2) );
 
 %% Multi R coding section
 REC_SIG_2 = MultiScaleFC( sig, scaleOrder, Fin_out, lambda );
