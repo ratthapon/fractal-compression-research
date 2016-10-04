49edda75556c3cce76415b45cef79aed0bb57796
Ratthapon <ratthapon_h@outlook.com>
2016-10-04 12:24:56 +0700

    Add comparation figure for MFCC comparation.

---
+++ Visualize/plotCMPMFCC.m
@@ -0,0 +1,31 @@
+function plotCMPMFCC( fig, varargin )
+%PLOTCMPMFCC Plot the features, multiple features can plot
+%simultaneousely.
+% fig - output figure number
+% varargin{i} - spectrum data
+% varargin{i + 1} - title
+
+figure(fig),
+set(fig, 'Position', [100, 100, 700, 300]);
+nSubplot = (nargin - 1)/2;
+for fIdx = 1:nSubplot
+    featData = varargin{(fIdx - 1)*2 + 1};
+    featName = varargin{(fIdx - 1)*2 + 2};
+    
+    subplot(1, nSubplot, fIdx), imagesc(-featData); 
+    set(gca, 'YDir', 'normal');
+    
+    %% set the figure's title and axes name
+    title(featName);
+    xlabel('Time (ms)');
+    ylabel('Coefficient index');
+    
+    %% set the axis tick information
+    ax = gca;
+    set(ax, 'XTickLabel', [1:6]*320);
+    set(ax, 'YTick', [1:5:30]);
+    set(ax, 'YTickLabel', [1:5:30]);
+    
+    colormap(gray);
+end
+
