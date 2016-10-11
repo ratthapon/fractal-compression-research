27f8096979410ad762d3c9869de95d9b8f611524
ratthapon_h <ratthapon_h@outlook.com>
2016-10-12 02:08:24 +0700

    Change dispatch batch ADPDecode operation to new version (v2).

--- SphinxBatchUtils/wavbatchbuilder/dispatchBatchADPMATLABDecode.m
+++ SphinxBatchUtils/wavbatchbuilder/dispatchBatchADPMATLABDecode.m
@@ -7,12 +7,12 @@ fileList = importdata( fullfile(expDirectory, 'an4traintest.txt') );
 inExt = 'mat';
 outExt = 'raw';
 outFs = 16000;
-RBS = [{'4'}, {'128'}];
+RBS = [{'4'}, {'64'}];
 minRBS = RBS{1};
 maxRBS = RBS{end};
 
 % permute paramters
-DATASET = [{'ADPFCMATLABRBS'}];
+DATASET = [{'ADPv2RBS'}];
 FS = [{8}, {16}];
 P = buildParamsMatrix( DATASET, FS );
 
@@ -22,7 +22,7 @@ for pIdx = 1:length(P)
     
     %% prepare reconstruct MRBS signals using MATLAB decode
     inFs = fs * 1000;
-    inDir = ['F:\IFEFSR\AudioFC\FC\AN4' num2str(fs) '_ADP_8_75_QR_GPU_' minRBS 'T' maxRBS];
+    inDir = ['F:\IFEFSR\AudioFC\FC\AN4' num2str(fs) '_ADPv2_RBS' minRBS 'T' maxRBS];
     outDir = fullfile(expDirectory, [ dataSet minRBS 'T' maxRBS 'FS' num2str(fs) '16']);
     batchMATLABDecode(  fileList, inDir, outDir, inFs, outFs, inExt, outExt  );
     
