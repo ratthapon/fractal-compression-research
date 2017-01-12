3d22d48c5b29fe108f4b927f39d950ccbb7e58be
ratthapon_h <ratthapon_h@outlook.com>
2017-01-12 11:50:12 +0700

    Fix path of LP dataset generation

--- Exp/TestLPEffect.m
+++ Exp/TestLPEffect.m
@@ -24,7 +24,7 @@ for pIdx = 1:size(P, 1)
     %% apply low-pass filter to recon rbs n 16->16
     inDir = ['F:\IFEFSR\ExpSphinx\' dataSet '16' '\wav'];
     outDir = fullfile(expDirectory, [dataSet 'T3LP' num2str(cutoff*8000) ...
-        'N' num2str(nFilt) 'FS' num2str(fs)]);
+        'N' num2str(nFilt) 'FS' num2str(fs) '\wav\']);
     %     batchMATLABResample2( fileList, inDir, outDir, nFilt, cutoff/(fs/8), inExt, outExt );
     batchMATLABResample3( fileList, inDir, outDir, 16000, fs*1000, inExt, outExt, nFilt, cutoff )
     
