e73ac3dc349e78f004d7245f15e32b517f664edf
ratthapon_h <ratthapon_h@outlook.com>
2017-07-01 03:11:36 +0700

    Formatted cepstrum x-tick labels

--- GraphicBuilder/Thesis/Slide/ComplexHarmonicSignal.m
+++ GraphicBuilder/Thesis/Slide/ComplexHarmonicSignal.m
@@ -17,8 +17,6 @@ stem(inSpec)
 
 % gen signal from spectrum
 sig = real(ifft(inSpec, f0/2, 1));
-figure(1), subplot(2,1,1)
-plot(sig)
 figure(2), subplot(3,1,1)
 plot(sig)
 
@@ -40,15 +38,31 @@ stem(ceps)
 datOutSig = [(1:size(sig))'*(1000/fs) sig];
 datOutSpec = [(1:f0)'*df abs(outSpec)];
 datOutSpec = datOutSpec(1:(f0/2), :);
-datOutCeps = [(1:cn)' abs(ceps) (ones(cn, 1)*(fs*2))./(1:cn)'];
+datOutCeps = [(1:cn)', abs(ceps), (1:cn)'/fs/2*1000, (ones(cn, 1)*(fs*2))./(1:cn)'];
 
 % plot with real axis tick
-figure(3), subplot(3,1,1)
+figure(1), subplot(3,1,1),
 plot(datOutSig(:, 1), datOutSig(:, 2))
-figure(3), subplot(3,1,2)
+title('Harmonized signal');
+xlabel('Time (ms)'), ylabel('Amplitude');
+
+figure(1), subplot(3,1,2)
 stem(datOutSpec(:, 1), datOutSpec(:, 2))
-figure(3), subplot(3,1,3)
+title('Spectrum');
+xlabel('Frequency (Hz)'), ylabel('Magnitude');
+
+figure(1), subplot(3,1,3)
 stem(datOutCeps(:, 1), datOutCeps(:, 2))
-set(gca, 'xtick', (1:16:cn)')
-set(gca, 'xticklabel', datOutCeps(1:16:cn, 3))
+% build label
+nTickRatio = 16;
+concatTick = @(a, b, c) sprintf('%s\n%s\n%s', ...
+    num2str(a), num2str(b), num2str(c));
+cepsTick = arrayfun(concatTick, datOutCeps(1:nTickRatio:cn, 1), datOutCeps(1:nTickRatio:cn, 3), ...
+    datOutCeps(1:nTickRatio:cn, 4), 'uniformoutput', false);
+set(gca, 'xtick', (1:nTickRatio:cn)')
+format_ticks(gca, cepsTick)
+xlabel({'','','','Top: Quefrency (Sample), Middle: Delay (ms), Bottom: Frequency (Hz)'}), 
+ylabel('Magnitude');
+
+
 
